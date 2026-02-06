319a980bf1708b3084bc98068cbd0ddefb0d8391
Rada <sirada_dee@cmu.ac.th>
2026-02-06 19:30:16 +0700

    lab16_1.cpp completed

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -32,3 +32,38 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void randData( double *dPtr, int N, int M ){
+    for(int i = 0; i < N*M; i++){
+        *(dPtr + i) = (rand() % 101) / 100.0 ;
+    }
+}
+
+void showData( double *dPtr, int N, int M ){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(dPtr + i) ;
+        if((i + 1) % M == 0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void findRowSum(const double *dPtr, double *sum1, int N, int M) {
+    for (int i = 0; i < N; i++) {
+        double rowSum = 0;
+        for (int j = 0; j < M; j++) {
+            rowSum += *(dPtr + (i * M + j));
+        }
+        sum1[i] = rowSum;
+    }
+}
+
+void findColSum(const double *dPtr, double *sum2, int N, int M) {
+    for (int j = 0; j < M; j++) sum2[j] = 0;
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            sum2[j] += *(dPtr + (i * M + j));
+        }
+    }
+}
